f98fea850790273017b9961b374d1b71c5386cd8
Bertrand PRESLES <bertrand.presles@niji.fr>
2017-06-15 15:19:44 +0200

    Removed GREP_OPTIONS --color=auto (deprecated)

--- aliases.zsh
+++ aliases.zsh
@@ -91,8 +91,6 @@ PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 #Increase history size
 #export HISTSIZE=1000
 #export HISTFILESIZE=1000
-#Use GREP color features by default
-export GREP_OPTIONS='--color=auto'
 #if [ -f /opt/local/etc/bash_completion ]; then
    #. /opt/local/etc/bash_completion
 #fi
